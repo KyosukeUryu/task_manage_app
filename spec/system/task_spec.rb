d2d43d872609614cb7e47acd672d41b09836f890
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-18 17:43:21 +0900

    [add]system spec実施

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :system do
+  describe 'タスク一覧画面' do
+    context 'タスクを作成した場合' do
+      it '作成済みのタスクが表示されること' do
+        task = FactoryBot.create(:task, name: 'task')
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+
+  describe 'タスク登録画面' do
+    context '必要項目を入力して、createボタンを押した場合' do
+      it 'データが保存されること' do
+        visit new_task_path
+        fill_in 'Name', with: 'hoge'
+        fill_in 'Description', with: 'fuga'
+        click_on '登録する'
+        expect(page).to have_content 'hoge'
+        expect(page).to have_content 'fuga'
+      end
+    end
+  end
+
+  describe 'タスク詳細画面' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示されたページに遷移すること' do
+         task1 = FactoryBot.create(:task, name: 'task1')
+         task2 = FactoryBot.create(:task, name: 'task2')
+         visit tasks_path
+         click_on 'task1'
+         expect(page).to have_content 'task1'
+         expect(page).not_to have_content 'task2'
+       end
+     end
+  end
+end
