431b90f9f7ebf632153e90ed1a9443a6d7a2d043
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-19 11:00:01 +0900

    [update]並び順のテスト完了、共通化に伴う他のテスト改良

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,11 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :system do
   describe 'タスク一覧画面' do
+    before do
+      FactoryBot.create(:task)
+      FactoryBot.create(:second_task)
+    end
+
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示されること' do
-        task = FactoryBot.create(:task, name: 'task')
         visit tasks_path
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'test_name'
+      end
+    end
+
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日付の降順に並んでいること' do
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'hoge'
+        expect(task_list[1]).to have_content 'test_name'
       end
     end
   end
@@ -15,8 +28,8 @@ RSpec.describe Task, type: :system do
     context '必要項目を入力して、createボタンを押した場合' do
       it 'データが保存されること' do
         visit new_task_path
-        fill_in 'Name', with: 'hoge'
-        fill_in 'Description', with: 'fuga'
+        fill_in 'タスク名', with: 'hoge'
+        fill_in 'タスク詳細', with: 'fuga'
         click_on '登録する'
         expect(page).to have_content 'hoge'
         expect(page).to have_content 'fuga'
@@ -27,12 +40,12 @@ RSpec.describe Task, type: :system do
   describe 'タスク詳細画面' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移すること' do
-         task1 = FactoryBot.create(:task, name: 'task1')
-         task2 = FactoryBot.create(:task, name: 'task2')
+         task1 = FactoryBot.create(:task)
+         task2 = FactoryBot.create(:second_task)
          visit tasks_path
-         click_on 'task1'
-         expect(page).to have_content 'task1'
-         expect(page).not_to have_content 'task2'
+         click_on 'test_name'
+         expect(page).to have_content 'test_name'
+         expect(page).not_to have_content 'hoge'
        end
      end
   end
