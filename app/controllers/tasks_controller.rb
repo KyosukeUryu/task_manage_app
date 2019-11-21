2a3da021c4e7090aee404b677fc09562f05eed7b
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-21 16:04:44 +0900

    [update]Task新規投稿にpriority追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :deadline, :status)
+    params.require(:task).permit(:name, :description, :deadline, :status, :priority)
   end
 
   def set_task
