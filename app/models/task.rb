94d4121e616392cbdd2169a8251ddfd7d7211d95
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-21 13:16:22 +0900

    [update]検索機能修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true
-  enum status: [:not_ready, :ready, :done]
+  enum status: [:"未着手", :"着手中", :"完了"]
 
   # def self.search(search)
   #   if search
