902e486d6d919a1a38fdb17911eb6f890e1ffecc
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-21 17:13:24 +0900

    [update]priority検索機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,29 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
   def index
-    if params[:sort_expired].present?
-      @tasks = Task.sorting
-    elsif params[:task].blank?
-      @tasks = Task.standard
-    else
-      if params[:task][:name].present? && params[:task][:status].present?
-        @tasks = Task.both_search(params[:task][:name], params[:task][:status])
-      elsif params[:task][:name].present?
-        @tasks = Task.name_search(params[:task][:name])
-      else
-        @tasks = Task.status_search(params[:task][:status])
-      end
+    # if params[:sort_expired].present?
+    #   @tasks = Task.sorting
+    # elsif params[:task].blank?
+    #   @tasks = Task.standard
+    # else
+    #   if params[:task][:name].present? && params[:task][:status].present?
+    #     @tasks = Task.both_search(params[:task][:name], params[:task][:status])
+    #   elsif params[:task][:name].present?
+    #     @tasks = Task.name_search(params[:task][:name])
+    #   else
+    #     @tasks = Task.status_search(params[:task][:status])
+    #   end
+    # end
+
+    @tasks = Task.standard
+    @tasks = Task.sorting if params[:sort_expired]
+    if params[:task]
+      @tasks = Task.name_search(params[:task][:name])
+      @tasks = @tasks.status_search(params[:task][:status]) if params[:task][:status].present?
+      @tasks = @tasks.priority_search(params[:task][:priority]) if params[:task][:priority].present?
+      
     end
+
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,9 +4,10 @@ class Task < ApplicationRecord
   enum status: [:"未着手", :"着手中", :"完了"]
   enum priority: [:"高", :"中", :"低"]
 
-  scope :both_search, -> (name, status){where('name LIKE ? AND status = ?', "%#{name}%", status )}
+  # scope :both_search, -> (name, status){where('name LIKE ? AND status = ?', "%#{name}%", status )}
   scope :name_search, -> (name){where('name LIKE ?', "%#{name}%")}
   scope :status_search, -> (status){where('status = ?', status)}
+  scope :priority_search, -> (priority){where('priority = ?', priority)}
   scope :sorting, -> {order(deadline: :desc)}
   scope :standard, -> {order(created_at: :desc)}
 end
