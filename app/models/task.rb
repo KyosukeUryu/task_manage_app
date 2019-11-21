f6f3ea03f52ffed094417cb7490359214e11a704
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-21 15:58:15 +0900

    [add]tasksテーブルにpriorityカラム追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,14 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true
   enum status: [:"未着手", :"着手中", :"完了"]
+  enum priority: [:"高", :"中", :"低"]
 
-  # def self.search(search)
-  #   if search
-  #     where(['name LIKE ?', "%#{search}%"])
-  #   else
-  #     all
-  #   end
-  # end
   scope :both_search, -> (name, status){where('name LIKE ? AND status = ?', "%#{name}%", status )}
   scope :name_search, -> (name){where('name LIKE ?', "%#{name}%")}
   scope :status_search, -> (status){where('status = ?', status)}
