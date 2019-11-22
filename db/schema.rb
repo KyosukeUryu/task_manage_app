c00f039d4479c8ea117859313a02c5448484eb26
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-22 13:47:45 +0900

    [add]Userモデル作成、マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_065219) do
+ActiveRecord::Schema.define(version: 2019_11_22_042023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_11_21_065219) do
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 0, null: false
     t.index ["name"], name: "index_tasks_on_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
