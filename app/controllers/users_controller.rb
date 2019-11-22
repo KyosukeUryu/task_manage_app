d5ed5e9b03915836a73c611d01cfece21a0abdf9
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-22 15:49:28 +0900

    [add]他人のユーザ詳細画面閲覧制限機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
