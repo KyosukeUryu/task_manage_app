0a1da6e7ad0ee91ba996f0f89250b22668920013
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-22 15:08:08 +0900

    [add]ログイン機能追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to tasks_path, notice: 'ログインしました'
+    else
+      flash.now[:danger] = 'ログインに失敗しましたs'
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :tasks
 
   resources :users, only: %i[new create show]
+
+  resources :sessions, only: %i[new create destroy]
 end
