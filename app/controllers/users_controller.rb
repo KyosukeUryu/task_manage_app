42bd76126ff6f151506ddf35d8004215d4878a63
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-22 19:50:13 +0900

    [update]ログイン後の新規登録画面制限、新規登録時のログイン機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def new
+    redirect_to root_path if logged_in?
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: '登録が完了しました'
     else
       render :new
