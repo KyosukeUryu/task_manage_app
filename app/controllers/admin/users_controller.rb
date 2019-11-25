32e0ef7fd0db06beb4c6462baefa2cdeea9f6d29
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-25 10:31:11 +0900

    [add]adminのuser作成機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,9 +5,16 @@ class Admin::UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: '新規ユーザーを作成しました'
+    else
+      render :new
+    end
   end
 
   def show
@@ -26,5 +33,9 @@ class Admin::UsersController < ApplicationController
 
   def admin_only
     redirect_to root_path unless current_user.try(:admin?)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
