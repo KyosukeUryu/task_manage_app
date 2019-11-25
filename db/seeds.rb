2c87234fd385cd0c3bc2e5f77d3a395918a0feb4
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-25 19:51:58 +0900

    [update]seedデータ更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-20.times do
-  Task.create!(
-              name: 'hoge',
-              description: 'fuga',
-              deadline: Time.now,
-              status: 0,
-              priority: 0,
-              user_id: 1
-  )
-end
+User.create!(
+            name: 'sample',
+            email: 'sample@hoge.com',
+            password: 'password',
+            password_confirmation: 'password',
+            admin: true
+)
