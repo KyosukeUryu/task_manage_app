09c5156465bf07eff64de66e17cd547a05d60b5f
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-25 19:49:28 +0900

    [update]admin の削除のみ対応するようコールバック修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, on: :update, allow_blank: true
 
   before_destroy do
-    if User.where(admin: true).count == 1
-      errors.add :base, '少なくとも1つ、ログイン用の認証が必要です'
+    if User.where(admin: true).count == 1 && self.admin?
       throw(:abort)
     end
   end
