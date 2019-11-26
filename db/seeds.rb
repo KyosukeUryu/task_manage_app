b09d3b64d5fd40d1c5a888fa39adc4d4d1730917
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-26 17:49:36 +0900

    [update]テスト用データ準備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,32 @@
-Label.create!(name: '検討課題')
-Label.create!(name: '難しい')
+User.create!(
+            name: "admin",
+            email: "admin@hoge.com",
+            password: "password",
+            password_confirmation: "password",
+            admin: true
+)
+
+
+10.times do |i|
+  User.create!(
+              name: "hoge#{i}",
+              email: Faker::Internet.email,
+              password: "password",
+              password_confirmation: "password",
+              admin: false
+  )
+
+  Task.create!(
+              name: "hoge#{i}",
+              description: "fuga#{i}",
+              deadline: Time.now,
+              status: rand(0..2),
+              priority: rand(0..2),
+              user_id: User.find(i + 1).id
+  )
+
+end
+
+Label.create!(
+              name: "テスト中"
+)
