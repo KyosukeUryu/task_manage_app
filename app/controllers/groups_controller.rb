17e074f48cc61ee14d04ecfb103bdf23a1496347
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-05 10:59:40 +0900

    [update]オーナーの自動登録、退会制限を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
     if @group.save
+      current_user.joins.create(group_id: @group.id)
       redirect_to groups_path, notice: 'グループを作成しました'
     else
       render :new
--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,11 +1,15 @@
 class JoinsController < ApplicationController
   def create
-    join = current_user.joins.create(group_id: params[:group_id])
+    current_user.joins.create(group_id: params[:group_id])
     redirect_to groups_path, notice: 'グループに参加しました!'
   end
 
   def destroy
-    join = current_user.joins.find_by(id: params[:id]).destroy
-    redirect_to groups_path, notice: 'グループから外れました'
+    if Join.find_by(id: params[:id]).group.owner_id == current_user.id
+      redirect_to groups_path, notice: 'オーナーは退会出来ません'
+    else
+      current_user.joins.find_by(id: params[:id]).destroy
+      redirect_to groups_path, notice: 'グループから外れました'
+    end
   end
 end
