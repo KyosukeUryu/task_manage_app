7e717beb474a053e530eeb71e5a9db223b644d90
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-08 15:55:43 +0900

    [update]rubocop修正中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,26 +5,8 @@ class TasksController < ApplicationController
   before_action :recommend_login
   def index
     @tasks = current_user.tasks.standard.page(params[:page]).per(10)
-    if params[:sort_expired]
-      @tasks = current_user.tasks.sorting.page(params[:page]).per(10)
-    end
-    if params[:sort_priority]
-      @tasks = current_user.tasks.sorting_priority.page(params[:page]).per(10)
-    end
-    if params[:task]
-      @tasks = @tasks.name_search(params[:task][:name]).page(params[:page]).per(10)
-      if params[:task][:status].present?
-        @tasks = @tasks.status_search(params[:task][:status]).page(params[:page]).per(10)
-      end
-      if params[:task][:priority].present?
-        @tasks = @tasks.priority_search(params[:task][:priority]).page(params[:page]).per(10)
-      end
-      if params[:task][:labels_id].present?
-        @labellings = Labelling.where('label_id = ?', params[:task][:labels_id])
-        @labelled_tasks_id = @labellings.pluck(:task_id)
-        @tasks = @tasks.where(id: @labelled_tasks_id)
-      end
-    end
+    sort_click
+    task_search_click
   end
 
   def new
@@ -72,6 +54,31 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def sort_click
+    if params[:sort_expired]
+      @tasks = current_user.tasks.sorting.page(params[:page]).per(10)
+    end
+    if params[:sort_priority]
+      @tasks = current_user.tasks.sorting_priority.page(params[:page]).per(10)
+    end
+  end
+
+  def task_search_click
+    if params[:task]
+      @tasks = @tasks.name_search(params[:task][:name]).page(params[:page]).per(10)
+      if params[:task][:status].present?
+        @tasks = @tasks.status_search(params[:task][:status]).page(params[:page]).per(10)
+      end
+      if params[:task][:priority].present?
+        @tasks = @tasks.priority_search(params[:task][:priority]).page(params[:page]).per(10)
+      end
+      if params[:task][:labels_id].present?
+        @labellings = Labelling.where('label_id = ?', params[:task][:labels_id])
+        @labelled_tasks_id = @labellings.pluck(:task_id)
+        @tasks = @tasks.where(id: @labelled_tasks_id)
+      end
+    end
+  end
   # def integer_string?(str)
   #   Integer(str)
   #   true
