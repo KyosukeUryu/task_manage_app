a3cd4d8a04fc83c9f8728d4faacca331d99c451c
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-08 15:12:00 +0900

    Fix 'Style/SafeNavigation'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to tasks_path, notice: 'ログインしました'
     else
